7790d26ab5a1582aa728a5bf150da67fccf11ad0
Raj Ghodasara <71821708+rajghodasara1@users.noreply.github.com>
2023-10-23 02:02:48 -0400

    Updated .travis.yml

--- polls/tests.py
+++ polls/tests.py
@@ -9,8 +9,7 @@ from django.utils import timezone
 class QuestionModelTests(TestCase):
     def test_was_published_in_future_with_future_question(self):
         """
-        was_published_recently returns False for questions
-        whose pub_date is in the future.
+        was_published_recently returns False for questions whose pub_date is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=1)
         future_question = Question(pub_date=time)
@@ -18,17 +17,15 @@ class QuestionModelTests(TestCase):
 
     def test_was_published_in_future_with_recent_question(self):
         """
-        was_published_recently returns True for questions
-        whose pub_date is within the past day.
+        was_published_recently returns True for questions whose pub_date is within the past day.
         """
-        time = timezone.now() - datetime.timedelta(days=1)
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertEqual(recent_question.was_published_recently(), True)
 
     def test_was_published_in_future_with_old_question(self):
         """
-        was_published_recently returns False for questions
-        whose pub_date is older than 1 day.
+        was_published_recently returns False for questions whose pub_date is older than 1 day.
         """
         time = timezone.now() - datetime.timedelta(days=1, minutes=1)
         old_question = Question(pub_date=time)
